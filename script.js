bce71aa7c26eae675f3fb66f2a9b613e6ec3e2ff
Dreamy Developer <158747343+Dream-World-Coder@users.noreply.github.com>
2024-04-03 19:46:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const cursor = document.querySelector('.cursor');
+    document.addEventListener('mousemove', (e) =>{
+        cursor.style.left = e.pageX + 'px';
+        cursor.style.top = e.pageY + 'px';
+  })
